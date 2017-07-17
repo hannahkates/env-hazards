1cecdeced25d371b217c09a050ca628b093a2ee3
Hannah Kates <hannah.b.kates@gmail.com>
2017-07-17 10:46:46 -0400

    created generic load remove functions

---
+++ js/config.js
@@ -0,0 +1,152 @@
+var layers = {
+  bldgOil: {
+    url: 'https://hbk254.carto.com/api/v2/sql?q=SELECT * FROM clean_heat_data&format=geojson',
+    type: 'geo',
+    category: 'air',
+    markerType: 'point',
+    markerStyle: {
+      radius: 4,
+      fillColor: 'black',
+      color: "#000",
+      weight: 1,
+      opacity: 1,
+      fillOpacity: 0.7
+    },
+    legendStyle: {
+
+    },
+    popupContent: {
+
+    },
+    divContent:
+      `<div>
+        <input id="check-bldgOil" type="checkbox" data-layer-name="bldgOil">
+        <span class="circle" style:{background=#f00, width=200px, height=200px, border-radius=50%}></span>
+        <span data-toggle="modal" data-target="#bldgOil" class="glyphicon glyphicon-info-sign"></span>
+        Buildings Burning Fuel Oil #4 and #6
+      </div>
+      <div class="modal fade" id="bldgOil" role="dialog">
+        <div class="modal-dialog">            
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal">&times;</button>
+              <h4 class="modal-title">Buildings Burning Fuel Oil #4 and #6</h4>
+            </div>
+            <div class="modal-body">
+              <img src="https://raw.githubusercontent.com/hannahkates/env-hazards/master/img/spotthesoot.jpg" width='100%'>
+              <p><i>View of 26 Broadway in Manhattan, the <a href="https://en.wikipedia.org/wiki/26_Broadway">Standard Oil Building</a></i></p>
+              <p>"New Yorkers burn more than 1 billion gallons of heating oil every year which accounts for nearly 14% of fine particulate matter pollutants emitted into our air; more PM2.5 emissions than all cars and trucks in the city combined. This particulate matter contains many pollutants that are associated with respiratory and cardiac diseases."<br>- <a href="http://www.nyc.gov/html/dep/html/air/buildings_heating_oil.shtml">NYC Dept. of Environmental Protection</a><p>
+              <p>This dataset provideds a comprehensive list of all buildings that still burn heavy heating oil. Any building burning #6 oil is in violation of NYC <a href="http://www.nyc.gov/html/dep/pdf/air/ll43.pdf">Local Law 43</a>. Data was last updated November 2015. Data Source: <a href="https://www.nyccleanheat.org/spot-the-soot">NYC Retrofit Accelerator</a></p>
+            </div>
+          </div>              
+        </div>
+      </div>`
+  },
+  cso: {
+    url: 'https://data.ny.gov/resource/5d4q-pk7d.json?dec_region=2',
+    type: 'nonGeo',
+    lat: 'latitude', 
+    long: 'longtitude',
+    category: 'water',
+    markerType: 'point',
+    markerStyle: {
+      radius: 4,
+      fillColor: '#FFA500',
+      color: "#000",
+      weight: 1,
+      opacity: 1,
+      fillOpacity: 0.9
+    },
+    legendStyle: {
+
+    },
+    popupContent: ['Wastewater Treatment Catchment: ', 'facility_name'],
+    divContent:
+      `<div>
+        <input id="check-cso" type="checkbox" data-layer-name="cso">
+        <span data-toggle="modal" data-target="#cso" class="glyphicon glyphicon-info-sign"></span>
+        Combined Sewer Outfalls
+      </div>
+      <div class="modal fade" id="cso" role="dialog">
+        <div class="modal-dialog">            
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal">&times;</button>
+              <h4 class="modal-title">Combined Sewer Overflow (CSO) Outfalls</h4>
+            </div>
+            <div class="modal-body">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/CSO_diagram_US_EPA.jpg" width="100%">
+              <p>"An E-Designation is a NYC zoning map designation that indicates the presence of an environmental requirement pertaining to potential Hazardous Materials Contamination, Window/Wall Noise Attenuation, or Air Quality impacts on a particular tax lot."<br>- <a href="http://www.nyc.gov/html/oer/html/e-designation/about.shtml">NYC Office of Environmental Remediation</a><p>
+              <p>This dataset captures all tax lots with an E-Designation. Data was last updated September 2016. Data Source: <a href="http://www1.nyc.gov/site/planning/data-maps/open-data/dwn-pluto-mappluto.page">MapPLUTO</a></p>
+            </div>
+          </div>              
+        </div>
+      </div>`
+  },
+  eDesig: {
+    url: 'https://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/MAPPLUTO/FeatureServer/0/query?where=EDesigNum%20IS%20NOT%20NULL&outFields=Address,EDesigNum&outSR=4326&f=geojson#features/3/properties',
+    type: 'geo',
+    category: 'contaminated',
+    markerType: 'polygon',
+    markerStyle: {
+      color: '#4f1111',
+      weight: 2,
+      opacity: 1
+    },
+    legendStyle: {
+
+    },
+    popupContent: {
+
+    },
+    divContent:
+      `<div>
+        <input id="check-eDesig" type="checkbox" data-layer-name="eDesig">
+        <span data-toggle="modal" data-target="#eDesig" class="glyphicon glyphicon-info-sign"></span>
+        E-Designation Sites
+      </div>
+      <div class="modal fade" id="eDesig" role="dialog">
+        <div class="modal-dialog">     
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal">&times;</button>
+              <h4 class="modal-title">E-Designation Sites</h4>
+            </div>
+            <div class="modal-body">
+              <img src="http://www.nyc.gov/html/oer/includes/site_images/misc/E%20Logo%20100%20x%20100.jpg">
+              <p>"An E-Designation is a NYC zoning map designation that indicates the presence of an environmental requirement pertaining to potential Hazardous Materials Contamination, Window/Wall Noise Attenuation, or Air Quality impacts on a particular tax lot."<br>- <a href="http://www.nyc.gov/html/oer/html/e-designation/about.shtml">NYC Office of Environmental Remediation</a><p>
+              <p>This dataset captures all tax lots with an E-Designation. Data was last updated September 2016. Data Source: <a href="http://www1.nyc.gov/site/planning/data-maps/open-data/dwn-pluto-mappluto.page">MapPLUTO</a></p>
+            </div>
+          </div>              
+        </div>
+      </div>`
+  },
+  three: {
+    url: "https://data.cityofnewyork.us/resource/fhrw-4uyv.json?agency=DEP&$limit=800&$where=complaint_type <> 'Noise' AND latitude>0",
+    type: 'nonGeo',
+    lat: 'latitude', 
+    long: 'longitude',
+    category: 'complaints',
+    markerType: 'point',
+    markerStyle: {
+      radius: 4,
+      fillColor: '#9b42f4',
+      color: "#000",
+      weight: 1,
+      opacity: 1,
+      fillOpacity: .9
+    },
+    legendStyle: {
+
+    },
+    popupContent: {
+
+    },
+    divContent:
+      `<div>
+        <input id="check-three" type="checkbox" data-layer-name="three">
+        <span data-toggle="modal" data-target="" class="glyphicon glyphicon-info-sign"></span>
+        Complaints to DEP - Most recent 800
+      </div>`
+  }
+}
--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 $(function() {
+  
+  // Rendering data layers list in left pane
+  for (var key in layers) {
+    var obj = layers[key];
+    $('#'+layers[key].category).append($(layers[key].divContent));
+  }
 
+  // Hiding loading gif
   var loading = $('#loading');
   loading.hide();
 
@@ -15,192 +22,66 @@ $(function() {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>'
   }).addTo(map);
 
-
-
-  var mapFunctions = {
-    csoGet: csoGet,
-    csoRemove: csoRemove,
-
-    bldgOilGet: bldgOilGet,
-    bldgOilRemove: bldgOilRemove,
-
-    threeGet: threeGet,
-    threeRemove: threeRemove,
-
-    eDesigGet: eDesigGet,
-    eDesigRemove: eDesigRemove
-  }
-
-  var layers = {
-    bldgOil: {
-      label: 'Buildings Burning Fuel Oil #4 and #6',
-      url: '',
-      type: 'geo',
-      layerName: '',
-      markerStyle: {
-        radius: 4,
-        fillColor: 'black',
-        color: "#000",
-        weight: 1,
-        opacity: 1,
-        fillOpacity: 0.7
-      },
-      legendStyle: {
-
-      },
-      popupContent: {
-
-      },
-      modalContent: {
-
-      }
-    },
-    cso: {
-      label: '',
-      url: '',
-      type: 'nonGeo',
-      layerName: '',
-      markerStyle: {
-        radius: 4,
-        fillColor: '#FFA500',
-        color: "#000",
-        weight: 1,
-        opacity: 1,
-        fillOpacity: 0.9
-      },
-      legendStyle: {
-
-      },
-      popupContent: {
-
-      },
-      modalContent: {
-
-      }
-    },
-    eDesig: {
-      label: '',
-      url: '',
-      type: 'geo',
-      layerName: '',
-      markerStyle: {
-        color: '#4f1111',
-        weight: 2,
-        opacity: 1
-      },
-      legendStyle: {
-
-      },
-      popupContent: {
-
-      },
-      modalContent: {
-
-      }
-    },
-    three: {
-      label: '',
-      url: '',
-      type: 'nonGeo',
-      layerName: '',
-      markerStyle: {
-        radius: 4,
-        fillColor: '#9b42f4',
-        color: "#000",
-        weight: 1,
-        opacity: 1,
-        fillOpacity: .9
-      },
-      legendStyle: {
-
-      },
-      popupContent: {
-
-      },
-      modalContent: {
-
-      }
-    }
-  }
-
+  // Functions to run when a layer checkbox is clicked
   function mapLayerClickFn(e) {
     var layerName = $(this).data('layer-name');
-    // console.log(layerName);
+    console.log(layers[layerName].url);
 
     if (this.checked) {
-      mapFunctions[layerName + 'Get']();
+      if(layers[layerName].type == 'nonGeo') {
+        getNonGeo(layerName);
+      } else if (layers[layerName].type == 'point'){
+        getGeoPoint(layerName);
+      } else {
+        getGeoPolygon(layerName);        
+      }
+      // mapFunctions[layerName + 'Get']();
     }  else {
-      mapFunctions[layerName + 'Remove']();
+      Remove(layerName);
+      // mapFunctions[layerName + 'Remove']();
     }
     $('.legend').remove();
-    generateLegend();
+    // generateLegend();
   }
 
-  $('#check-cso').change(mapLayerClickFn)
-  $('#check-bldgOil').change(mapLayerClickFn)
-  $('#check-eDesig').change(mapLayerClickFn)
-  $('#check-three').change(mapLayerClickFn)
+  // Checking if a checkbox gets clicked
+  $('input').change(mapLayerClickFn);
 
-
-  var generateLegend = function() {
-    var legend = L.control({position: 'topright'});
-    legend.onAdd = function (map) {
-      function getColor(d) {
-      return d > 1000 ? '#800026' :
-        d > 500  ? '#BD0026' :
-        d > 200  ? '#E31A1C' :
-        d > 100  ? '#FC4E2A' :
-        d > 50   ? '#FD8D3C' :
-        d > 20   ? '#FEB24C' :
-        d > 10   ? '#FED976' : '#FFEDA0';
-      }
-      var div = L.DomUtil.create('div', 'info legend'),
-        grades = layers,
-        labels = [];
-      // loop through our density intervals and generate a label with a colored square for each interval
-      for (var i = 0; i < layers.length; i++) {
-          div.innerHTML +=
-            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-      }
-      return div;
-    };
-    legend.addTo(map);
-  }
-
-  // Adding CSO Outfall Locations
-  var csoLayer = L.layerGroup([]);
-  function csoGet() {
+  // Function for getting NON-geojson layers
+  function getNonGeo(layerName) {
     loading.show();
-    var csoURL = 'https://data.ny.gov/resource/5d4q-pk7d.json?dec_region=2';
-    $.getJSON(csoURL, function(data){
+    layers[layerName].layer = L.layerGroup([]);
+    var lat = layers[layerName].lat; // Gets field name for latitude (inconsistent across datasets)
+    var long = layers[layerName].long; // Gets field name for longitude (inconsistent across datasets)
+    var url = layers[layerName].url;
+    $.getJSON(url, function(data){
       for(var i=0; i<data.length; i++) {
         var marker = data[i];
-        var geojsonMarkerOptions  = layers['cso'].markerStyle;
-        var csoPoint = L.circleMarker( [data[i].latitude, data[i].longtitude], geojsonMarkerOptions ).bindPopup(
-          data[i].facility_name
-        );
-        csoLayer.addLayer(csoPoint);
+        var popupContent = layers[layerName].popupContent[0];
+        console.log(popupContent);
+        for(var j=1; j<layers[layerName].popupContent.length; j++){
+          popupContent += data[layers[layerName].popupContent[j]];
+        }
+        var geojsonMarkerOptions  = layers[layerName].markerStyle;
+        var point = L.circleMarker( [data[i][lat], data[i][long]], geojsonMarkerOptions ).bindPopup(popupContent);
+        layers[layerName].layer.addLayer(point);
       }
-      csoLayer.addTo(map);
+      layers[layerName].layer.addTo(map);
       loading.hide();
     });
   }
-  function csoRemove() {
-    map.removeLayer(csoLayer);
-  }
 
-  var bldgOilLayer;
-  // Adding fuel oil building data
-  function bldgOilGet() {
+  // Function for getting geojson POINT layers
+  function getGeoPoint(layerName) {
     loading.show();
-    var bldgOilURL = 'https://hbk254.carto.com/api/v2/sql?q=SELECT * FROM clean_heat_data&format=geojson';
+    layers[layerName].layer = L.geoJson();
+    var url = layers[layerName].url;
 
-    $.getJSON(bldgOilURL, function(sitePoint) {
-      bldgOilLayer = L.geoJson(sitePoint, {
+    $.getJSON(url, function(sitePoint) {
+      layers[layerName].layer = L.geoJson(sitePoint, {
         pointToLayer: function (feature, latlng) {
             var d = feature.properties; 
-            var geojsonMarkerOptions = layers['bldgOil'].markerStyle;
+            var geojsonMarkerOptions = layers[layerName].markerStyle;
             return L.circleMarker(latlng, geojsonMarkerOptions);
         },
         onEachFeature: function(feature, layer) {
@@ -212,21 +93,19 @@ $(function() {
             layer.bindPopup(popupText);
         }
       })
-      bldgOilLayer.addTo(map);
+      layers[layerName].layer.addTo(map);
       loading.hide();
     });
   }
-  function bldgOilRemove() {
-    map.removeLayer(bldgOilLayer);
-  }
 
-  // Adding E-Designation Properties
-  var eDesigLayer;
-  function eDesigGet() {
+  // Function for getting geojson POLYGON layers
+  function getGeoPolygon(layerName) {
     loading.show();
-    var eDesigURL = 'https://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/MAPPLUTO/FeatureServer/0/query?where=EDesigNum%20IS%20NOT%20NULL&outFields=Address,EDesigNum&outSR=4326&f=geojson#features/3/properties';
-    $.getJSON(eDesigURL, function(sitePoint) {
-      eDesigLayer = L.geoJson(sitePoint, {
+    layers[layerName].layer = L.geoJson();
+    var url = layers[layerName].url;
+
+    $.getJSON(url, function(sitePoint) {
+      layers[layerName].layer = L.geoJson(sitePoint, {
         style: layers['eDesig'].markerStyle,
         onEachFeature: function(feature, layer) {
             var d = feature.properties;   
@@ -235,34 +114,141 @@ $(function() {
             layer.bindPopup(popupText);
           }
       })
-      eDesigLayer.addTo(map);
+      layers[layerName].layer.addTo(map);
       loading.hide();
     });
   }
-  function eDesigRemove() {
-    map.removeLayer(eDesigLayer);
+
+  // Remove any layer
+  function Remove(layerName){
+    map.removeLayer(layers[layerName].layer);
   }
 
-  // Adding 311 complaints data
-  var threeLayer = L.layerGroup([]);
-  function threeGet() {
-    loading.show();
-    var threeURL = "https://data.cityofnewyork.us/resource/fhrw-4uyv.json?agency=DEP&$limit=800&$where=complaint_type <> 'Noise' AND latitude>0";
-    $.getJSON(threeURL, function(data){
-      for(var i=0; i<data.length; i++) {
-        var marker = data[i];
-        var geojsonMarkerOptions = layers['three'].markerStyle;
-        var threePoint = L.circleMarker( [data[i].latitude, data[i].longitude], geojsonMarkerOptions ).bindPopup(
-          data[i].descriptor
-        );
-        threeLayer.addLayer(threePoint);
-      }
-      threeLayer.addTo(map);
-      loading.hide();
-    });
-  }
-  function threeRemove() {
-    map.removeLayer(threeLayer);
-  }
+  // function csoRemove() {
+  //   map.removeLayer(csoLayer);
+  // }
 
+
+  // Adding CSO Outfall Locations
+  // var csoLayer = L.layerGroup([]);
+  // function csoGet() {
+  //   loading.show();
+  //   var csoURL = 'https://data.ny.gov/resource/5d4q-pk7d.json?dec_region=2';
+  //   $.getJSON(csoURL, function(data){
+  //     for(var i=0; i<data.length; i++) {
+  //       var marker = data[i];
+  //       var geojsonMarkerOptions  = layers['cso'].markerStyle;
+  //       var csoPoint = L.circleMarker( [data[i].latitude, data[i].longtitude], geojsonMarkerOptions ).bindPopup(
+  //         data[i].facility_name
+  //       );
+  //       csoLayer.addLayer(csoPoint);
+  //     }
+  //     csoLayer.addTo(map);
+  //     loading.hide();
+  //   });
+  // }
+  // function csoRemove() {
+  //   map.removeLayer(csoLayer);
+  // }
+
+  // var bldgOilLayer;
+  // // Adding fuel oil building data
+  // function bldgOilGet() {
+  //   loading.show();
+  //   var bldgOilURL = 'https://hbk254.carto.com/api/v2/sql?q=SELECT * FROM clean_heat_data&format=geojson';
+
+  //   $.getJSON(bldgOilURL, function(sitePoint) {
+  //     bldgOilLayer = L.geoJson(sitePoint, {
+  //       pointToLayer: function (feature, latlng) {
+  //           var d = feature.properties; 
+  //           var geojsonMarkerOptions = layers['bldgOil'].markerStyle;
+  //           return L.circleMarker(latlng, geojsonMarkerOptions);
+  //       },
+  //       onEachFeature: function(feature, layer) {
+  //           var d = feature.properties;   
+  //           var popupText = d.street_address + '<br />'
+  //             + 'Fuel: ' + d.primary_fuel + '<br />'
+  //             + 'Estimated Retirement Year: ' + d.est_retirement_year + '<br />'  
+  //             + 'Owner: ' + d.owner;
+  //           layer.bindPopup(popupText);
+  //       }
+  //     })
+  //     bldgOilLayer.addTo(map);
+  //     loading.hide();
+  //   });
+  // }
+  // function bldgOilRemove() {
+  //   map.removeLayer(bldgOilLayer);
+  // }
+
+  // // Adding E-Designation Properties
+  // var eDesigLayer;
+  // function eDesigGet() {
+  //   loading.show();
+  //   var eDesigURL = 'https://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/MAPPLUTO/FeatureServer/0/query?where=EDesigNum%20IS%20NOT%20NULL&outFields=Address,EDesigNum&outSR=4326&f=geojson#features/3/properties';
+  //   $.getJSON(eDesigURL, function(sitePoint) {
+  //     eDesigLayer = L.geoJson(sitePoint, {
+  //       style: layers['eDesig'].markerStyle,
+  //       onEachFeature: function(feature, layer) {
+  //           var d = feature.properties;   
+  //           var popupText = d.Address + '<br />'
+  //             + 'E-Designation Number: ' + d.EDesigNum;
+  //           layer.bindPopup(popupText);
+  //         }
+  //     })
+  //     eDesigLayer.addTo(map);
+  //     loading.hide();
+  //   });
+  // }
+  // function eDesigRemove() {
+  //   map.removeLayer(eDesigLayer);
+  // }
+
+  // // Adding 311 complaints data
+  // var threeLayer = L.layerGroup([]);
+  // function threeGet() {
+  //   loading.show();
+  //   var threeURL = "https://data.cityofnewyork.us/resource/fhrw-4uyv.json?agency=DEP&$limit=800&$where=complaint_type <> 'Noise' AND latitude>0";
+  //   $.getJSON(threeURL, function(data){
+  //     for(var i=0; i<data.length; i++) {
+  //       var marker = data[i];
+  //       var geojsonMarkerOptions = layers['three'].markerStyle;
+  //       var threePoint = L.circleMarker( [data[i].latitude, data[i].longitude], geojsonMarkerOptions ).bindPopup(
+  //         data[i].descriptor
+  //       );
+  //       threeLayer.addLayer(threePoint);
+  //     }
+  //     threeLayer.addTo(map);
+  //     loading.hide();
+  //   });
+  // }
+  // function threeRemove() {
+  //   map.removeLayer(threeLayer);
+  // }
+
+  // var generateLegend = function() {
+  //   var legend = L.control({position: 'topright'});
+  //   legend.onAdd = function (map) {
+  //     function getColor(d) {
+  //     return d > 1000 ? '#800026' :
+  //       d > 500  ? '#BD0026' :
+  //       d > 200  ? '#E31A1C' :
+  //       d > 100  ? '#FC4E2A' :
+  //       d > 50   ? '#FD8D3C' :
+  //       d > 20   ? '#FEB24C' :
+  //       d > 10   ? '#FED976' : '#FFEDA0';
+  //     }
+  //     var div = L.DomUtil.create('div', 'info legend'),
+  //       grades = layers,
+  //       labels = [];
+  //     // loop through our density intervals and generate a label with a colored square for each interval
+  //     for (var i = 0; i < layers.length; i++) {
+  //         div.innerHTML +=
+  //           '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+  //           grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+  //     }
+  //     return div;
+  //   };
+  //   legend.addTo(map);
+  // }
 })
