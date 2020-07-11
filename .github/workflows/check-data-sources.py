00196aec95eaa89d7ea7c64da837720bfc332d6f
Hannah Kates <hannah.b.kates@gmail.com>
2020-07-11 19:44:25 -0400

    clean up python script

--- .github/workflows/check-data-sources.py
+++ .github/workflows/check-data-sources.py
@@ -1,12 +1,12 @@
 import urllib.request, json 
 
-# test url with data
+# Test url with data
 url = "https://hbk254.carto.com/api/v2/sql?q=SELECT%20*%20FROM%20clean_heat_data%20WHERE%20RIGHT(primary_fuel,1)%20=%20'6'&format=geojson"
 
-# test url with zero records
+# Test url with zero records
 # url = "https://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/MAPPLUTO/FeatureServer/0/query?where=EDesigNum%20IS%20NOT%20NULL&outFields=Address,EDesigNum&outSR=4326&f=geojson#features/3/properties"
 
-# test url that is broken
+# Test url that is broken
 # url = "https://planninglabs.carto.com/api/v2/sql?q=SELECT * FROM facdb_170522 WHERE facsubgrp = 'Solid Waste Transfer and Carting'&format=geojson"
 
 def check_if_data_source_works(url):
@@ -17,8 +17,3 @@ def check_if_data_source_works(url):
 	return(1/len(data['features']))
 
 check_if_data_source_works(url)
-
-# data_source_urls = ['']
-
-# for i in data_source_urls:
-# 	check_if_data_source_works(i) 
